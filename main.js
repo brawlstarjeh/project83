d95ce28c4e226aabc71b5068ce584d4cecb3fe8e
brawlstarjeh <75985911+brawlstarjeh@users.noreply.github.com>
2021-04-05 12:51:15 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+
+    ctx.strokestyle = "grey";
+    ctx.lineWidth = 4;
+    ctx.rect(150, 143, 430, 200);
+    ctx.stroke();
+
+    ctx.beginpath();
+    ctx.strokestyle = "blue";
+    ctx.lineWidth = 5;
+    ctx.arc(330, 210, 40, 0, 2*Math.PI);
+    ctx.stroke();
+    
